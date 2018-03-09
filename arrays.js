81d359cbd618a060245fcd5296e95f1892ad83c7
Clarence Kwong <clarencekwong7@gmail.com>
2018-03-09 21:21:21 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -9,3 +9,13 @@ function destructivelyAddElementToBeginningOfArray (list,item) {
   list.unshift(item)
   return list
 }
+
+function addElementToBeginningOfArray (list,item) {
+  const list2 = [item,...list]
+  return list2
+}
+
+function destructivelyAddElementToBeginningOfArray (list,item) {
+  list.unshift(item)
+  return list
+}
